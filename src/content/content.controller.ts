480c7beb403f62b70e87ff29d82a4631d25d0704
Kadir Kaplan <kadir.kaplan@trt.net.tr>
2025-12-05 00:49:00 +0300

    Update content.controller.ts

--- src/content/content.controller.ts
+++ src/content/content.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { ContentService } from './content.service';
 import { Content } from '../schemas/content.schema';
 import { contentFilterDto, paginationDto } from './dto/content-filter.dto';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 
 @Controller('content')
 export class ContentController {
   constructor(private readonly contentService: ContentService) { }
 
 
-
+  //@UseGuards(JwtAuthGuard)
   @Post()
   async findAll(
     @Body() filter: contentFilterDto,
@@ -17,6 +18,7 @@ export class ContentController {
     return await this.contentService.findAll(filter, pagination);
   }
 
+  //@UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.contentService.findOne(id);
